eb9f2d288ded9e52d1beeca0f9c9a30a9f40afcc
domhuff <dom.think21@gmail.com>
2019-08-22 21:21:44 -0600

    MAST-ML config

--- Regress.py
+++ Regress.py
@@ -15,7 +15,10 @@ def rmsle (y_pred, y_test):
     return np.sqrt(np.mean((np.log(1+y_pred)- np.log(1+y_test))**2))
 
 data_train = pd.read_csv('train-houses.csv')
+dup_data = data_train
 # data_test = pd.read_csv('test-houses.csv')
+all_columns = data_train.columns
+
 features = list()
 
 quant_features = ['OpenPorchSF', 'WoodDeckSF', '2ndFlrSF', 'MasVnrArea', 'YearBuilt', 'LotArea', 'GarageCars', 'TotalBsmtSF', 'SalePrice']
@@ -35,31 +38,47 @@ train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.2, random_st
 #############
 # Linear Regression Model
 #############
-lin_model= LinearRegression().fit(train_X, train_y)
-yHat= lin_model.predict(val_X)
+# lin_model= LinearRegression().fit(train_X, train_y)
+# yHat= lin_model.predict(val_X)
+#
+# print('R^2 for Linear Regression model: ' , r2_score(val_y, yHat))
+# print('RMSLE for Linear Regression model: ', rmsle(yHat, val_y))
+#
+#
+# #############
+# # Random Forest Model
+# #############
+# forest_model = RandomForestRegressor(n_estimators=100, max_depth=20, random_state=1)
+# forest_model.fit(train_X, train_y)
+# forest_predicts = forest_model.predict(val_X)
+#
+# print('R^2 for Random Forest Regression model: ' , r2_score(val_y, forest_predicts))
+# print('RMSLE for Random Forest Regression model: ', rmsle(forest_predicts, val_y))
+#
+#
+# #############
+# # Support Vector Machine Model
+# #############
+# svc_model = SVC(gamma = 'scale', random_state=1)
+# svc_model.fit(train_X, train_y)
+# svc_predicts = svc_model.predict(val_X)
+#
+# print('R^2 for SVM Regression model: ' , r2_score(val_y, svc_predicts))
+# print('RMSLE for SVM Regression model: ', rmsle(svc_predicts, val_y))
+def non_numeric(data, col_name):
+    data[col_name] = data[col_name].map(
+        {10: 'Very Excellent', 9: 'Excellent', 8: 'Very Good', 7: 'Good', 6: 'Above Average', 5: 'Average',
+         4: 'Below Average', 3: 'Fair', 2: 'Poor', 1: 'Very Poor',    20:'1-STORY 1946', 30: '1-STORY 1945 & OLDER', 40: '1-STORY W/FINISHED A', 45: '1-1/2 STORY - UNFIN', 50: '1-1/2 STORY FINIS', 60: '2-STORY 1946 & NEWER', 70: '2-STORY 1945 & OLDER', 75: '2-1/2 STORY ALL AGES', 80: 'SPLIT OR MULTI-LEVEL', 85: 'SPLIT FOYER', 90: 'DUPLEX - ALL STYLES AND AGES', 120: '1-STORY PUD (Planned U', 150: '1-1/2 STORY PUD - ALL ', 160: '2-STORY PUD - 1946 &', 180: 'PUD - MULTILEVEL - INCL SPLI', 190: '2 FAMILY CONVERSION - ALL STY'})
+    return data
 
-print('R^2 for Linear Regression model: ' , r2_score(val_y, yHat))
-print('RMSLE for Linear Regression model: ', rmsle(yHat, val_y))
+tmplist = ['OverallCond', 'OverallQual', 'MSSubClass']
+for col in tmplist:
+    dup_data = non_numeric(dup_data, col)
+all_categ = dup_data.iloc[:,[1, 2, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,22,23,24,25,27,28,29,30,31,32,33,35,39,40,41,42,53,55,57,58,60,63,64,65,72,73,74,78,79]].columns
 
-
-#############
-# Random Forest Model
-#############
-forest_model = RandomForestRegressor(n_estimators=100, max_depth=20, random_state=1)
-forest_model.fit(train_X, train_y)
-forest_predicts = forest_model.predict(val_X)
-
-print('R^2 for Random Forest Regression model: ' , r2_score(val_y, forest_predicts))
-print('RMSLE for Random Forest Regression model: ', rmsle(forest_predicts, val_y))
-
-
-#############
-# Support Vector Machine Model
-#############
-svc_model = SVC(gamma = 'scale', random_state=1)
-svc_model.fit(train_X, train_y)
-svc_predicts = svc_model.predict(val_X)
-
-print('R^2 for SVM Regression model: ' , r2_score(val_y, svc_predicts))
-print('RMSLE for SVM Regression model: ', rmsle(svc_predicts, val_y))
-
+# print(dup_data.columns)
+for cols in all_categ:
+    dup_data = make_categor_dum_var(dup_data, cols)
+dup_data['Price'] = dup_data.SalePrice
+dup_data = dup_data.drop(columns = 'SalePrice')
+dup_data.to_csv(r'~/house_full_quant.csv', index =False)
